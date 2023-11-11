0d4d6439b5dbbe2452975d38ada2350d803e3066
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-11-11 16:19:32 +0100

    fix eslint problem

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,22 @@
-'use client'
+'use client';
 
-import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
-import { ConnectKitProvider } from "connectkit";
-import { config } from '../wagmi'
+import * as React from 'react';
+import { WagmiConfig } from 'wagmi';
+import { ConnectKitProvider } from 'connectkit';
+import { config } from '../wagmi';
 
 type ProvidersProps = Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>;
 
-export function Providers({ children }: ProvidersProps)  {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
-  return <WagmiConfig config={config}><ConnectKitProvider theme="retro">{mounted && children}</ConnectKitProvider></WagmiConfig>
+export function Providers({ children }: ProvidersProps) {
+	const [mounted, setMounted] = React.useState(false);
+	React.useEffect(() => setMounted(true), []);
+	return (
+		<WagmiConfig config={config}>
+			<ConnectKitProvider theme="retro">
+				{mounted && children}
+			</ConnectKitProvider>
+		</WagmiConfig>
+	);
 }
--- wagmi.ts
+++ wagmi.ts
@@ -1,27 +1,25 @@
-import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { getDefaultConfig } from "connectkit";
-import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, createConfig } from 'wagmi';
+import { goerli, mainnet } from 'wagmi/chains';
+import { getDefaultConfig } from 'connectkit';
+import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-  [
-    publicProvider(),
-  ],
-)
+	[mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+	[publicProvider()]
+);
 
 export const config = createConfig(
-  getDefaultConfig({
-    // Required API Keys
-    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+	getDefaultConfig({
+		// Required API Keys
+		alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID, // or infuraId
+		walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
 
-    // Required
-    appName: "NexBridge",
+		// Required
+		appName: 'NexBridge',
 
-    // Optional
-    appDescription: "NexGen Bridge",
-    appUrl: "https://www.nexarb.com", // your app's url
-    appIcon: "https://www.nexarb.com./img/nexbridge.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  }),
-)
+		// Optional
+		appDescription: 'NexGen Bridge',
+		appUrl: 'https://www.nexarb.com', // your app's url
+		appIcon: 'https://www.nexarb.com./img/nexbridge.png', // your app's icon, no bigger than 1024x1024px (max. 1MB)
+	})
+);
