97373ffe339f53ae0ca8843f6e1a574d2e09f02f
trax07 <61095643+trax07@users.noreply.github.com>
2023-11-16 01:00:36 +0300

    Folder structure is rearranged

--- platform/app/about/page.tsx
+++ platform/app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import AppBar from '@/components/CustomNavbar';
+import AppBar from '@/platform/components/CustomNavbar';
 
 const About = () => {
     return (
