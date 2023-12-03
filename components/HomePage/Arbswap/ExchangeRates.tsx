8b4e968504f8b6a0bc193f03d1227589f31d4079
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-12-03 19:21:57 +0100

    Add real price data

--- components/HomePage/Arbswap/ExchangeRates.tsx
+++ components/HomePage/Arbswap/ExchangeRates.tsx
@@ -1,84 +1,131 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import redstone from "redstone-api";
+import { ResponsiveLine } from '@nivo/line'
+
+type Coin = {
+  symbol: string,
+  name: string,
+  color: string
+}
+
+interface PriceChartData {
+  x: string;
+  y: number;
+}
+
+interface PriceChart {
+  id: string;
+  color: string;
+  data: PriceChartData[];
+}
 
 const ExchangeRates = (): React.JSX.Element => {
   return (
     <div className="mt-72">
-        <p className="text-5xl text-center">Exchange Rates</p>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-3 gap-3 w-2/3 mt-10 p-10">
-            <div className="bg-indigo-950 w-80 rounded-2xl p-5">
-              <div className="flex justify-between">
-                <div>Bitcoin</div>
-                <div className="text-sm opacity-50">SOL/BTC</div>
-              </div>
-              <div className="mt-8">
-                <p className="text-2xl">0,00079 BTC</p>
-              </div>
-              <div className="flex justify-between mt-6">
-                <div className="bg-orange-600 rounded-full py-1.5 px-5">
-                  +0.23%
-                </div>
-                <div>
-                  <Image
-                    src={'/img/bitcoin-curve.png'}
-                    alt='Bitcoin Curve'
-                    width={100}
-                    height={31}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="bg-indigo-950 w-80 rounded-2xl p-5">
-              <div className="flex justify-between">
-                <div>Ethereum</div>
-                <div className="text-sm opacity-50">SOL/ETH</div>
-              </div>
-              <div className="mt-8">
-                <p className="text-2xl">0,013920 ETH</p>
-              </div>
-              <div className="flex justify-between mt-6">
-                <div className="bg-purple-700 rounded-full py-1 px-4">
-                  +0.23%
-                </div>
-                <div>
-                  <Image
-                    src={'/img/ethereum-curve.png'}
-                    alt='Ethereum Curve'
-                    width={106}
-                    height={31}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="bg-indigo-950 w-80 rounded-2xl p-5">
-              <div className="flex justify-between">
-                <div>USDT</div>
-                <div className="text-sm opacity-50">USDT/SOL</div>
-              </div>
-              <div className="mt-8">
-                <p className="text-2xl">$24,342</p>
-              </div>
-              <div className="flex justify-between mt-6">
-                <div className="bg-blue-500 rounded-full py-1 px-4">
-                  +0.23%
-                </div>
-                <div>
-                  <Image
-                    src={'/img/usdt-curve.png'}
-                    alt='USDT Curve'
-                    width={101}
-                    height={31}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+      <p className="text-5xl text-center">Exchange Rates</p>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-3 gap-3 w-2/3 mt-10 p-10">
+          <PriceCard symbol='BTC' name='Bitcoin' color='bg-orange-600' />
+          <PriceCard symbol='SOL' name='Solana' color='bg-purple-700' />
+          <PriceCard symbol='ETH' name='Ethereum' color='bg-blue-500' />
         </div>
-        <div className="flex justify-center mt-14">
-        </div>
-        
+      </div>
+      <div className="flex justify-center mt-14">
+      </div>
+
     </div>
+  )
+}
+
+const PriceCard = ({ symbol, name, color }: Coin) => {
+  const [price, setPrice] = useState("");
+  const [lowestPrice, setLowestPrice] = useState("")
+  const [priceChartData, setPriceChartData] = useState<PriceChart[]>([])
+  useEffect(() => {
+    const getPrice = async () => {
+      return await redstone.getPrice(symbol.toUpperCase());
+    }
+    const getPrices = () => {
+      return redstone.getHistoricalPrice(symbol, {
+        offset: 0,
+        limit: 100,
+      });
+    }
+    getPrice().then((res) => {
+      setPrice(res.value.toFixed(3));
+    });
+    getPrices().then((res) => {
+      setLowestPrice(res.sort((p1, p2) => p1.value < p2.value ? 1 : -1)[0].value.toFixed(3))
+      const _priceChartData = [];
+      const newData = {
+        data: res.filter((price)=> price.value != 0).slice(0,20).map((price) => ({
+          x: price.value.toFixed(2),
+          y: price.timestamp,
+        })),
+        id: res.length > 0 ? res[0].symbol : '', // Check if 'res' has elements before accessing 'symbol'
+        color: "hsl(133, 70%, 50%)",
+      };
+      _priceChartData.push(newData);
+      setPriceChartData(_priceChartData);
+    })
+  }, [])
+  return (
+    <div className="bg-indigo-950 w-80 rounded-2xl p-5">
+      <div className="flex justify-between">
+        <div>{name.toUpperCase()}</div>
+        <div className="text-sm opacity-50">{symbol.toUpperCase()}/USD</div>
+      </div>
+      <div className="mt-8">
+        <p className="text-2xl">{price} USD</p>
+      </div>
+      <div className="flex  justify-between mt-6">
+        <div className={`${color} rounded-full my-auto py-1.5 px-5`}>
+          {lowestPrice > price ? "-" : "+"} {(Math.abs(Number(lowestPrice) - Number(price)) / 100).toFixed(2)}%
+        </div>
+        <div className='w-20 h-14 my-auto'>
+          <Chart data={priceChartData}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+type ChartProps = {
+  data:PriceChart[]
+}
+
+const Chart = ({data}: ChartProps) => {
+  return (
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 'auto',
+        max: 'auto',
+        stacked: true,
+        reverse: false
+      }}
+      curve="cardinal"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={null}
+      axisLeft={null}
+      enableGridX={false}
+      enableGridY={false}
+      enablePoints={false}
+      pointSize={7}
+      pointColor={{ from: 'color', modifiers: [] }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabel="y"
+      pointLabelYOffset={-12}
+      isInteractive={false}
+      legends={[]}
+    />
   )
 }
 
