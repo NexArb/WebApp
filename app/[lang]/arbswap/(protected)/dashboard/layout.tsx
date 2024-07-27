62812522876f16d1738bd344be294acd77831f0c
trax07 <61095643+trax07@users.noreply.github.com>
2024-07-27 14:32:57 +0300

    Some changes made in anchor.ts file

--- app/[lang]/arbswap/(protected)/dashboard/layout.tsx
+++ app/[lang]/arbswap/(protected)/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ArbSwapNavbar from '@/components/CustomNavbar/ArbSwap'
 import { FormDataProvider } from '@/context/offerFormDataContext'
+import AppWalletProvider from '@/components/App/AppWalletProvider/AppWalletProvider'
 
 type RootLayoutProps = Readonly<{
   children: React.ReactNode
@@ -10,11 +11,13 @@ type RootLayoutProps = Readonly<{
 
 export default function DashboardLayout({ children, params }: RootLayoutProps) {
   return (
-    <FormDataProvider>
-      <section className="min-h-screen overflow-hidden bg-gradient-to-t from-purple-950 via-slate-900 to-slate-950">
-        <ArbSwapNavbar locale={params.lang} />
-        {children}
-      </section>
-    </FormDataProvider>
+    <AppWalletProvider>
+      <FormDataProvider>
+        <section className="min-h-screen overflow-hidden bg-gradient-to-t from-purple-950 via-slate-900 to-slate-950">
+          <ArbSwapNavbar locale={params.lang} />
+          {children}
+        </section>
+      </FormDataProvider>
+    </AppWalletProvider>
   )
 }
